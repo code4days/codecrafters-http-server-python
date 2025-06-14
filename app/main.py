f079d9a8d2b0587c4e76a1fec06ef035a863d426
Rasheed Elsaleh <rasheed.elsaleh@octo.us>
2025-06-14 14:37:27 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,6 +10,11 @@ class RequestLine:
     version: str
 
 
+@dataclass
+class RequestHeaders:
+    headers: dict[str, str]
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -18,12 +23,19 @@ def main():
     conn, _ = server_socket.accept()  # wait for client
     request = conn.recv(1024).decode()
 
+    request_headers = {
+        header.split(":")[0].strip(): header.split(":")[1].strip()
+        for header in request.split("\r\n")[1:-1]
+        if header
+    }
     rl = RequestLine(*request.split("\r\n")[0].split())
     if rl.target == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
     elif rl.target.startswith("/echo/"):
         response_body = rl.target.split("/echo/")[-1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
+    elif rl.target.startswith("/user-agent"):
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request_headers['User-Agent'])}\r\n\r\n{request_headers['User-Agent']}"
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     conn.sendall(response.encode())
