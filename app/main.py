56ac9e8e952f8c581f09c1995ed6b9bafd7ce257
Rasheed Elsaleh <rasheed.elsaleh@octo.us>
2025-06-17 23:09:14 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,46 +1,84 @@
 import socket  # noqa: F401
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import threading
 import sys
 from pathlib import Path
 
 
 @dataclass
-class RequestLine:
+class HttpRequest:
     method: str
     target: str
     version: str
+    headers: dict[str, str] = field(default_factory=dict)
+    body: bytes | None = None
 
 
-def handle_request(client_socket, storage_path):
+STATUS_OK = "HTTP/1.1 200 OK"
+STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found"
+STATUS_CREATED = "HTTP/1.1 201 Created"
+
+
+def construct_response(status_message: str, content_type: str, body: str) -> str:
+    response = f"{status_message}\r\n"
+    response += f"Content-Type: {content_type}\r\n"
+    response += f"Content-Length: {len(body)}\r\n\r\n"
+    response += body
+    return response
+
+
+def parse_request(request: str) -> HttpRequest:
+    request_parts = request.split("\r\n")
+    http_request = HttpRequest(*request_parts[0].split())
+    http_request.headers = {
+        header.split(":")[0].strip(): header.split(":")[1].strip()
+        for header in request_parts[1:-1]
+        if header
+    }
+    http_request.body = request_parts[-1]
+
+    return http_request
+
+
+def handle_request(client_socket: socket, storage_path: str) -> None:
     with client_socket:
         request = client_socket.recv(1024).decode()
+        http_request = parse_request(request)
 
-        rl = RequestLine(*request.split("\r\n")[0].split())
-        request_headers = {
-            header.split(":")[0].strip(): header.split(":")[1].strip()
-            for header in request.split("\r\n")[1:-1]
-            if header
-        }
+        if http_request.target == "/":
+            response = f"{STATUS_OK}\r\n\r\n"
 
-        if rl.target == "/":
-            response = "HTTP/1.1 200 OK\r\n\r\n"
-        elif rl.target.startswith("/echo/"):
-            response_body = rl.target.split("/echo/")[-1]
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
-        elif rl.target.startswith("/user-agent"):
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request_headers['User-Agent'])}\r\n\r\n{request_headers['User-Agent']}"
-        elif rl.target.startswith("/files"):
-            filename = rl.target.split("/files/")[-1]
-            try:
-                with open(Path(storage_path).joinpath(filename), "r") as f:
-                    file_contents = f.read()
-            except FileNotFoundError:
-                response = "HTTP/1.1 404 Not Found\r\n\r\n"
-            else:
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(file_contents)}\r\n\r\n{file_contents}"
+        elif http_request.target.startswith("/echo/"):
+            response_body = http_request.target.split("/echo/")[-1]
+            response = construct_response(STATUS_OK, "text/plain", response_body)
+
+        elif http_request.target.startswith("/user-agent"):
+            response = construct_response(
+                STATUS_OK, "text/plain", http_request.headers["User-Agent"]
+            )
+
+        elif http_request.target.startswith("/files"):
+            filename = http_request.target.split("/files/")[-1]
+            file_path = Path(storage_path).joinpath(filename)
+
+            if http_request.method == "GET":
+                try:
+                    with open(file_path, "r") as f:
+                        file_contents = f.read()
+                except FileNotFoundError:
+                    response = f"{STATUS_NOT_FOUND}\r\n\r\n"
+                else:
+                    response = construct_response(
+                        STATUS_OK, "application/octet-stream", file_contents
+                    )
+
+            if http_request.method == "POST":
+                request_body = request.split("\r\n")[-1]
+                with open(file_path, "w") as f:
+                    f.write(request_body)
+                response = f"{STATUS_CREATED}\r\n\r\n"
         else:
-            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            response = f"{STATUS_NOT_FOUND}\r\n\r\n"
 
         client_socket.sendall(response.encode())
 
@@ -55,7 +93,6 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         client_socket, client_address = server_socket.accept()  # wait for client
-        print(f"connected to {client_address}")
         t = threading.Thread(target=handle_request, args=(client_socket, storage_path))
         t.start()
 
